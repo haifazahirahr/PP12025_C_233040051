eb0f9d3d1d42ff128b1e96394da3254a31c4bee5
haifazahirahr <haifazahirahr5@gmail.com>
2025-02-13 11:33:13 +0700

    first commit

---
+++ src/pertemuan2/Node.java
@@ -0,0 +1,30 @@
+package pertemuan2;
+
+public class Node {
+	private int nilai;
+	private Node next;
+	
+	// inisialisasi Node
+	public Node(int nilai) {
+		this.nilai = nilai;
+		this.next = null;
+	}
+	
+	//setter & getter 
+	public void setNilai(int nilai) {
+        this.nilai = nilai;
+    }
+	
+	  public int getNilai() {
+	        return nilai;
+	    }
+	  
+	  public void setNext(Node next) {
+	        this.next = next;
+	    }
+
+	    public Node getNext() {
+	        return next;
+	    }
+}
+
